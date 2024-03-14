ffc88e1b6825a30b63b43077d553cb8541d9dc53
akhademik <hajtran@gmail.com>
2024-03-14 12:20:05 +0700

    feat: finished v1
    upload and try to fix error if any

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,16 @@
 const current_date = new Date()
-const CURRENT_YEAR = current_date.getFullYear()
+export const CURRENT_YEAR = current_date.getFullYear()
+
+const rate = {
+	above_1b: {
+		up_to_3: { r678: 0.99, r3678: 0.99 },
+		from_3_to_6: { r678: 1.1, r3678: 1.1 },
+	},
+	from_7m_to_1b: {
+		up_to_3: { r678: 1.11, r3678: 1.24 },
+		from_3_to_6: { r678: 1.3, r3678: 1.43 },
+	},
+}
 
 export const years_from_3_to_6 = () => {
 	const _result = []
@@ -9,7 +20,7 @@ export const years_from_3_to_6 = () => {
 	return _result
 }
 
-export const years_up_to_3 = () => {
+const years_up_to_3 = () => {
 	return years_from_3_to_6().splice(3)
 }
 
@@ -19,4 +30,20 @@ export const format_money_input = (input: string) => {
 		.replace(/^0+/, '') // remove leading zero
 		.replace(/\B(?=(\d{3})+(?!\d))/g, ',') // insert ',' every 3 digits for thousand separators
 	return input
+}
+
+export const get_quotation = (price: string, year: string, dkbs: 'r678' | 'r3678') => {
+	const _price = parseInt(price.replaceAll(',', ''))
+
+	if (_price < 700000000) return 'Gía trị xe thấp nhất 700tr'
+
+	const _above_1b = _price > 1000000000
+	const _up_to_3year = years_up_to_3().includes(parseInt(year))
+
+	const _rate = _above_1b ? rate.above_1b : rate.from_7m_to_1b
+	const _period = _up_to_3year ? 'up_to_3' : 'from_3_to_6'
+
+	const _result = Math.round((_price * _rate[_period][dkbs]) / 100)
+
+	return isNaN(_result) ? '' : `${'Phí : ' + format_money_input(_result.toString()) + ' vnđ'}`
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-	import { format_money_input, years_from_3_to_6, years_up_to_3 } from '$lib/utils'
+	import { CURRENT_YEAR, format_money_input, get_quotation, years_from_3_to_6 } from '$lib/utils'
 
 	let selected_year: string
 	let price = ''
+	let dkbs: 'r678' | 'r3678' = 'r3678'
 
 	$: price = format_money_input(price)
+	$: final_quote = get_quotation(price, selected_year, dkbs)
 </script>
 
 <div class="grid h-screen w-screen place-content-center bg-slate-500">
 	<div class="flex flex-col gap-3 rounded-lg border border-white p-4 text-white">
-		<h1 class="text-center">TÍNH PHÍ XE PVI</h1>
+		<h1 class="text-center text-2xl font-bold text-orange-400">TÍNH PHÍ XE PVI</h1>
+
 		<section class="flex justify-between gap-2">
 			<p>Giá trị xe:</p>
 			<input
@@ -23,34 +26,35 @@
 				class="w-36 bg-slate-400 px-2 text-right"
 				bind:value={selected_year}>
 				{#each years_from_3_to_6() as year (year)}
-					<option>{year}</option>
+					<option selected={year === CURRENT_YEAR}>{year}</option>
 				{/each}
 			</select>
 		</section>
 		<div class="flex flex-col gap-2">
 			<section class="flex flex-col">
+				<p class="py-2 text-center font-bold">Đăng Ký Bổ Sung</p>
 				<p>03: Bảo hiểm mất cắp bộ phận</p>
 				<p>06: Không khấu hao phụ tùng, vật tư thay mới</p>
 				<p>07: Bảo hiểm lựa chọn cơ sở sửa chữa</p>
 				<p>08: Bảo thiệt hại động cơ xe do bị thủy kích</p>
 			</section>
-			<section class="flex justify-evenly">
+			<section class="flex justify-evenly pt-2">
 				<div class="flex items-center gap-2">
 					<input
 						type="radio"
-						id="option1"
-						name="options"
-						checked />
-					<label for="option1">03, 06, 07, 08</label>
+						bind:group={dkbs}
+						value="r3678" />
+					<p>03, 06, 07, 08</p>
 				</div>
 				<div class="flex items-center gap-2">
 					<input
 						type="radio"
-						id="option2"
-						name="options" />
-					<label for="option2">06, 07, 08</label>
+						bind:group={dkbs}
+						value="r678" />
+					<p>06, 07, 08</p>
 				</div>
 			</section>
 		</div>
+		<p class="h-8 text-center text-2xl font-bold text-orange-400">{final_quote}</p>
 	</div>
 </div>
